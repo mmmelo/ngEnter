de34f7f73af5052a4d8150ed8069308703a7629e
Marcel Medeiros <marcel.mdr@gmail.com>
2015-12-10 09:11:08 -0500

    Directive to Enter

---
+++ ngEnter.js
@@ -0,0 +1,23 @@
+(function(){
+
+  'use strict'
+
+  angular
+  .module('ngEnter',[])
+  .directive('ngEnter', ngEnter);
+
+  function ngEnter(){
+    return function(scope, element, attrs) {
+       element.bind("keydown keypress", function(event) {
+           if(event.which === 13) {
+               scope.$apply(function(){
+                   scope.$eval(attrs.ngEnter, {'event': event});
+               });
+               event.preventDefault();
+           }
+       });
+   };
+  }
+
+
+})();
